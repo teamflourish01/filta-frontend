1785b2a4234d15e98429c6487cc1ad43865ddb96
RinaBhalani <rina.teamflourish@gmail.com>
2024-08-21 11:15:45 +0530

    first commit

---
+++ filta/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Mycard from './pages/mycard/Mycard';
+
+function App() {
+  return (
+    <div className="App">
+    <Mycard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ filta/src/pages/mycard/Mycard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Mycard() {
+  return (
+    <div>
+      MyCard
+    </div>
+  );
+}
+
+export default Mycard;
